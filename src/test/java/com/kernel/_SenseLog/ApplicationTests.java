1ae6ac50fe500a05a72714f218d22ddb10c5aac9
Yeju-Kang <bbogaeme@github.com>
2025-05-07 14:59:53 +0900

    Initialize project

---
+++ src/test/java/com/kernel/_SenseLog/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kernel._SenseLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
